75a16e4711a744fad80382adfc08a984f068b426
adhikaris3 <51837064+adhikaris3@users.noreply.github.com>
2020-12-20 18:56:26 -0600

    Add files via upload

---
+++ Admin-View/Customers/cus.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Connecting to database</title>
+    </head>
+    <body>
+        <?php
+            $con = mysqli_connect('localhost','root','');
+            $db = mysqli_select_db($con, 'Database');
+            if($con){
+                echo 'connected to database';
+            }
+            else{
+                die('error');
+            }
+        
+            if($db){
+                echo 'found database';
+            }
+            else{
+                die('error. no database');
+            }
+        ?>
+        <br />
+        <br />
+        <?php
+            $sql = mysqli_query($con, "SELECT * FROM Customer", MYSQLI_USE_RESULT);
+          
+            while($row = mysqli_fetch_array($sql)){
+                $customerID = $row['CustomerID'];
+                $fname = $row['Fname'];
+                $lname = $row['Lname'];
+                $username = $row['UserName'];
+                $password = $row['Password'];
+                $cuid = $row['CuID'];
+                
+                echo ' CustomerID: ' . $customerID . ' Fname: ' . $fname . ' Lname: ' . $lname . ' UserName: ' . $username .' Password: '. $password . ' CuID:' . $cuid.'<br />';
+            }
+           
+        ?>
+    </body>
+</html>
